35df43a0af18d2304ab14539093f9725d8adb2b4
Genius <lhadirahimil@gmail.com>
2023-04-22 22:19:53 +0430

    part 1

---
+++ app/src/main/java/com/hadirahimi/paypalredesign/ui/activity/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.hadirahimi.paypalredesign.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hadirahimi.paypalredesign.R
+
+class LoginActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState : Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/hadirahimi/paypalredesign/ui/fragment/WalletFragment.kt
@@ -0,0 +1,23 @@
+package com.hadirahimi.paypalredesign.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hadirahimi.paypalredesign.R
+
+
+class WalletFragment : Fragment()
+{
+    
+    
+    override fun onCreateView(
+        inflater : LayoutInflater , container : ViewGroup? , savedInstanceState : Bundle?
+    ) : View?
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wallet , container , false)
+    }
+    
+}
